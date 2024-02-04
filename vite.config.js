a1d4e228b1a6ac29bc70e8f69fd2765a72561a7a
hasaigal <hasaigal@gmail.com>
2024-02-04 16:15:28 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Challenge_07_bank_account",
+  base: "/Challenge_07_bank_account/",
   plugins: [react()],
 })
